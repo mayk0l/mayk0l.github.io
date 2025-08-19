44cc841ec44d5da37b4ac859db6ff57d369c1b97
mayk0l <maykol0219@gmail.com>
2025-08-18 23:38:25 -0400

    more style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ import typographyPlugin from '@tailwindcss/typography'
 export default defineConfig({
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily: {
         'sans': ['Ubuntu', 'sans-serif'],
